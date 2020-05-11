99498b42f82e14cb1bbf2e85a351d90062feec49
Andrew Ellis <h.drew.ellis@gmail.com>
2020-05-11 15:56:19 -0500

    week7/Day 1 Work

--- week7/day1/server/index.js
+++ week7/day1/server/index.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const app = express();
+const mustache = require('mustache-express')
 const port = 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-let list = [];
+app.engine('mustache', mustache())
+app.set('views', './views')
+app.set('view engine', 'mustache')
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: flase }))
+
+
+let list = [];
+
 
 app.get('/todos', function(req, res) {
-    res.json(list)
+    res.render('todos')
 });
 
 app.post('/todos', function(req, res) {
 
-    let id = req.body.id
     let title = req.body.title
     let priority = req.body.priority
     let DTG = req.body.DTG
 
     list.push({ title: title, priority: priority, date: DTG })
 
-    let succes = `Task of ${title} with a ${priority} priority has been added`
+    let success = `Task of ${title} with a ${priority} priority has been added`
 
-    res.json({ message: succes })
+    res.json({ message: success })
 });
 
 app.listen(port, function() {
