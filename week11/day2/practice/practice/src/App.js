52e8674dad0bbb6f2a102b7bb93fa759efa6063c
Andrew Ellis <h.drew.ellis@gmail.com>
2020-06-09 10:37:36 -0500

    created react stepper

---
+++ week11/day2/practice/practice/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Stepper from "./components/Stepper"
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+      <Stepper />
+      </div>
+    )
+  }
+  }
+
+
+export default App;
---
+++ week11/day2/practice/practice/src/components/Stepper/index.js
@@ -0,0 +1,39 @@
+import React, { Component} from "react";
+import "./Stepper.css"
+
+class Stepper extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        }
+
+        this.countUpButton = this.countUpButton.bind(this);
+        this.countDownButton = this.countDownButton.bind(this);
+    }
+
+    countUpButton() {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+    countDownButton() {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+
+    }
+    render() {
+
+        return(
+        <div>
+        <button className="minusButton" onClick= { this.countDownButton }>-</button>
+        <h2>{this.state.counter}</h2>
+        <button className="addButton" onClick= { this.countUpButton }>+</button>
+        </div>
+        )
+    }
+}
+
+export default Stepper
